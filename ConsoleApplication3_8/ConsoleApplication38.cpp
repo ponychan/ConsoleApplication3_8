132d970f13ab744d9bdcfe9474810c8e0f28a3a3
ponychan <rzsckbmx@163.com>
2017-10-06 11:56:42 +0800

    3.8.cpp
    递归调用求阶乘

--- ConsoleApplication3_8/ConsoleApplication38.cpp
+++ ConsoleApplication3_8/ConsoleApplication38.cpp
@@ -1,11 +1,27 @@
 // ConsoleApplication38.cpp: 定义控制台应用程序的入口点。
-//
+//3.8.cpp递归调用求阶乘
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+//计算N的阶乘，递归调用
+unsigned fac(unsigned n) {
+	unsigned f;
+	if (n == 0)
+		f = 1;
+	else
+		f = n* fac(n - 1);
+	return f;
+}
 
 int main()
 {
-    return 0;
+	unsigned n;
+	cout << "enter a positive integer:";
+	cin >> n;
+	unsigned y = fac(n);
+	cout << n << "!=" << y << endl;
+	return 0;
 }
 
